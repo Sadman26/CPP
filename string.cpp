309b925caf5ed73bc02576ceeaefe8d7db6939b2
Sadman26 <fareless.ranger@gmail.com>
2023-09-08 00:21:06 +0600

    Compare two string

---
+++ string.cpp
@@ -0,0 +1,19 @@
+// Assalamu Alaikum 💖
+// Bm.Shadman Sakib Mahee (Sadman26)
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string x1, x2;
+    x1 = "Sadman";
+    x2 = "Sadman";
+    if (!x1.compare(x2))
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+    return 0;
+}
