42e22ed80ddb38f9734ecd02eb398b263dcdab8a
Sadman26 <fareless.ranger@gmail.com>
2023-09-14 20:57:42 +0600

    Trying to Make Vector

---
+++ Stl_Vector.cpp
@@ -0,0 +1,14 @@
+// Assalamu Alaikum 💖
+// Bm.Shadman Sakib Mahee (Sadman26)
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> vtr;
+    vtr.push_back(10);
+    vtr.push_back(20);
+    vtr.push_back(30);
+    vtr.push_back(40);
+    vtr.push_back(50);
+    return 0;
+}
